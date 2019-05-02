837c9157fa538ef70e3e5587355f7955e4338ef9
kumachan0210 <kumachan0220ios@gmail.com>
2019-05-02 22:44:19 +0900

    あとしょーいさん…

--- themeDecision/themeDecision/Form1.cs
+++ themeDecision/themeDecision/Form1.cs
@@ -7,7 +7,10 @@ namespace themeDecision
     public partial class Form1 : Form
     {
 
-        private string[] theme = { "!", "現実を拡張せよ！", "放置してみていると楽しいもの\n（多少操作してもOK）", "ボール", "面倒くさいが楽になる", "4", "5" };
+        private string[] theme = { "!", "現実を拡張せよ！", "放置して見ていると楽しいもの\n（多少操作してもOK）",
+                                    "ボール", "面倒くさいが楽になる", "行列を楽しめるプロダクト   \n（ゲーム禁止）",
+                                    "5" };
+        private bool onlyone = false;
         public Form1()
         {
             InitializeComponent();
@@ -15,12 +18,16 @@ namespace themeDecision
 
         private void Clicked(object sender, EventArgs e)
         {
-            byte[] bs = new byte[4];
-            RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();
-            rand.GetBytes(bs);
-            rand.Dispose();
-            System.Diagnostics.Trace.WriteLine(BitConverter.ToUInt32(bs, 0));
-            textBox1.Text = theme[BitConverter.ToUInt32(bs, 0) % 7];
+            if (!onlyone)
+            {
+                byte[] bs = new byte[4];
+                RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();
+                rand.GetBytes(bs);
+                rand.Dispose();
+                System.Diagnostics.Trace.WriteLine(BitConverter.ToUInt32(bs, 0));
+                textBox1.Text = theme[BitConverter.ToUInt32(bs, 0) % 7];
+                onlyone = true;
+            }
         }
     }
 }
